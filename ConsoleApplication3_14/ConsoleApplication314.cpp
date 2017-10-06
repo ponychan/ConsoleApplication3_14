cac786daa8b338a20c2450304c1e7ec59d2793cd
ponychan <rzsckbmx@163.com>
2017-10-06 13:37:07 +0800

    3.14.cpp
    内联函数应用举例

--- ConsoleApplication3_14/ConsoleApplication314.cpp
+++ ConsoleApplication3_14/ConsoleApplication314.cpp
@@ -1,11 +1,22 @@
 // ConsoleApplication314.cpp: 定义控制台应用程序的入口点。
-//
+//3.14.cpp内联函数应用举例
 
 #include "stdafx.h"
+#include<iostream>
+using namespace std;
 
+const double PI = 3.14159265358979;
+
+//内联函数，根据圆的半径计算其面积
+inline double area(double r) {
+	return PI*r*r;
+}
 
 int main()
 {
-    return 0;
+	double r = 3.0;
+	double s = area(r);
+	cout << s << endl;
+	return 0;
 }
 
